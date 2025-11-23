37cf12d529866fe593ffe30fb76256d811dfc3f6
m3ru <merugopalan2007@gmail.com>
2025-11-23 05:13:12 -0500

    fix: match scripts

--- scripts/start_evaluation.sh
+++ scripts/start_evaluation.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-#SBATCH --time=0-10:0:00
-#SBATCH --account=def-vganesh
+#SBATCH --job-name=evaluation
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=10:00:00
 #SBATCH --mem=16G
-#SBATCH -o logs/evaluation_%j.log
+#SBATCH --output=logs/evaluation_%j.log
+#SBATCH -A gts-vganesh3
+#SBATCH -q inferno
 
-source ../../general/bin/activate
-PYTHONPATH=./src:$PYTHONPATH
+cd "$SLURM_SUBMIT_DIR"
+
+source "$HOME/.bashrc"
+conda activate llmsat
+
+export PYTHONPATH="./src:${PYTHONPATH:-}"
+
 python src/llmsat/pipelines/evaluation.py --run_all
