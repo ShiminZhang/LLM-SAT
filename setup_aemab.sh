c81a1a7eee3b9603dd91255d6a6b2c17db32f2c6
Mike Lu <mike020830@gmail.com>
2026-02-17 14:48:38 -0800

    update setup

--- setup_aemab.sh
+++ setup_aemab.sh
@@ -2,7 +2,7 @@
 # Setup AE_kissatMAB solver
 
 set -euo pipefail
-tar -xf AE_kissat2025_MAB.tar.xz -C ~/scratch/LLM-SAT/solvers/
+tar -xf AE_kissat2025_MAB.tar.xz -C solvers/
 mv solvers/AE_kissat2025_MAB/* solvers/base/
 rm -rf solvers/AE_kissat2025_MAB
 cd solvers/base
