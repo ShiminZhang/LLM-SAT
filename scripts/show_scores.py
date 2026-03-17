7e13123b88295e9c94eec4fa37332cbd805ac6a0
m3ru <merugopalan2007@gmail.com>
2026-03-17 11:14:56 -0400

    show scores script

---
+++ scripts/show_scores.py
@@ -0,0 +1,18 @@
+"""Show PAR2 scores for a generation tag.
+
+Usage: python scripts/show_scores.py <generation_tag>
+"""
+import sys
+from llmsat.utils.aws import get_ids_from_router_table, get_algorithm_result
+from llmsat.llmsat import CHATGPT_DATA_GENERATION_TABLE
+
+tag = sys.argv[1]
+ids = get_ids_from_router_table(CHATGPT_DATA_GENERATION_TABLE, tag)
+results = []
+for aid in ids:
+    a = get_algorithm_result(aid)
+    if a and a.raw_par2_score:
+        results.append((a.raw_par2_score[-1], a.role.value, aid))
+results.sort()
+for par2, role, aid in results:
+    print(f"{aid[:16]}  {role:<6}  PAR2={par2:.2f}")
